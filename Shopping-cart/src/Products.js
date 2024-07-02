3db09d9bc483639484bcb534c51da81bdc37e73b
Mosesvictor1 <federaluk0@gmail.com>
2024-07-02 11:55:34 +0100

    bed

--- Shopping-cart/src/Products.js
+++ Shopping-cart/src/Products.js
@@ -5,98 +5,98 @@ export const productsData = [
     id: 1,
     title: "moses",
     price: 2000,
-    imageUrl: "/Images/studio2.jpg",
+    imageUrl: "./images/300$_luxury-divan-base.jpg",
   },
 
   {
     id: 2,
     title: "Carton Astronaut T-Shirt",
     price: 300,
-    imageUrl: "/Images/studio2.jpg",
+    imageUrl: "./images/300$_luxury-divan-base.jpg",
   },
 
   {
     id: 3,
     title: "Carton Astronaut T-Shirt",
     price: 300,
-    imageUrl: "/Images/studio2.jpg",
+    imageUrl: "./images/300$_luxury-divan-base.jpg",
   },
 
   {
     id: 4,
     title: "Carton Astronaut T-Shirt",
     price: 95,
-    imageUrl: "/Images/studio2.jpg",
+    imageUrl: "./images/300$_luxury-divan-base.jpg",
   },
 
   {
     id: 5,
     title: "Carton Astronaut T-Shirt",
     price: 3500,
-    imageUrl: "/Images/studio2.jpg",
+    imageUrl: "./images/300$_luxury-divan-base.jpg",
   },
 
   {
     id: 6,
     title: "Carton Astronaut T-Shirt",
     price: 12,
-    imageUrl: "./picsturss/cccccc.jpg",
+    imageUrl: "picsturss/cccccc.jpg",
   },
 
   {
     id: 7,
     title: "Carton Astronaut T-Shirt",
     price: 500,
-    imageUrl: "./picsturss/ccccccc.jpg",
+    imageUrl: "picsturss/ccccccc.jpg",
   },
 
   {
     id: 8,
     title: "Carton Astronaut T-Shirt",
     price: 500,
-    imageUrl: "./picsturss/cccccccc.jpg",
+    imageUrl: "picsturss/cccccccc.jpg",
   },
 
   {
     id: 9,
     title: "Carton Astronaut T-Shirt",
     price: 129,
-    imageUrl: "./picsturss/ccccccccc.jpg",
+    imageUrl: "picsturss/ccccccccc.jpg",
   },
 
   {
     id: 10,
     title: "Carton Astronaut T-Shirt",
     price: 650,
-    imageUrl: "./picsturss/cccccccccc.jpg",
+    imageUrl: "picsturss/cccccccccc.jpg",
   },
 
   {
     id: 11,
     title: "Carton Astronaut T-Shirt",
     price: 700,
-    imageUrl: "./picsturss/chh.jpg",
+    imageUrl: "picsturss/chh.jpg",
   },
 
   {
     id: 12,
     title: "Carton Astronaut T-Shirt",
     price: 119,
-    imageUrl: "./picsturss/chhh.jpg",
+    imageUrl: "picsturss/chhh.jpg",
   },
 
   {
     id: 13,
     title: "Carton Astronaut T-Shirt",
     price: 400,
-    imageUrl: "./picsturss/cc.jpg",
+    imageUrl: "picsturss/cc.jpg",
   },
 
   {
     id: 14,
     title: "Carton Astronaut T-Shirt",
     price: 500,
-    imageUrl: "./picsturss/ccc.jpg",
+    imageUrl: "picsturss/ccc.jpg",
   },
 
   {
